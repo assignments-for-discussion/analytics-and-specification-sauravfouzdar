f45ead7d4f07d3836641cbaef2da32971fbdc582
sauravfouzdar <sauravfouzdar@gmail.com>
2021-12-10 12:29:49 +0530

    solution to calculate average of data obtained from sensors

--- average.js
+++ average.js
@@ -1,6 +1,19 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  //return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+
+  
+  var total = 0;// variable to store total sum of array:numbers
+  var count = 0;// total elements in array:numbers
+  var n = numbers.length;//length of array:numbers
+  for(let i=0;i<n;++i) {
+      if(isNaN(numbers[i])) continue;//check to skip NaN: not a Number
+      total += numbers[i];
+      count++;
+  }
+  
+  var average = ((total / count).toFixed(1));//rounded to 2 digits after decimal
+  return average; 
 }
 
 module.exports = {average};
