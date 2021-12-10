ce23e4a0be583f7d68756e9be6effc42d4d39835
sauravfouzdar <sauravfouzdar@gmail.com>
2021-12-10 12:50:23 +0530

    average of values obtained from sensors

--- average.js
+++ average.js
@@ -2,7 +2,7 @@
 function average(numbers) {
   //return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
 
-  
+
   var total = 0;// variable to store total sum of array:numbers
   var count = 0;// total elements in array:numbers
   var n = numbers.length;//length of array:numbers
@@ -12,7 +12,8 @@ function average(numbers) {
       count++;
   }
   
-  var average = ((total / count).toFixed(1));//rounded to 2 digits after decimal
+  //rounded to 2 digits after decimal
+  var average = ((total / count).toFixed(1));
   return average; 
 }
 
